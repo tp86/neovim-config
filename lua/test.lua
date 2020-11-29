0b8da718898d84764a40170237ee00d595da7062
tp86 <tom0palka+tp86@gmail.com>
2020-11-29 15:48:40 +0100

    Add defaults and module-returned table overwriting protection mechanisms

--- lua/test.lua
+++ lua/test.lua
@@ -1,6 +1,11 @@
-local cache = require"cacher"
+local defaults = {
+    components = {},
+    statusline = {},
+}
 
-local components = {}
+local components = setmetatable({}, {
+    __index = defaults.components
+})
 
 local function transform_set(tbl, transformer)
     return function(_, k, v)
@@ -11,6 +16,21 @@ local function transform_set(tbl, transformer)
     end
 end
 
+local function cache(tbl)
+    local cache_table = {}
+    return setmetatable(cache_table, {
+        __index = function(t, k)
+            local tbl_value = tbl[k]
+            local callable, result = pcall(tbl_value)
+            if callable then
+                tbl_value = result
+            end
+            rawset(t, k, tbl_value)
+            return rawget(t, k)
+        end
+    })
+end
+
 local function table_w_transform_r_cache(tbl)
     return {
         __newindex = transform_set(tbl),
@@ -18,21 +38,47 @@ local function table_w_transform_r_cache(tbl)
     }
 end
 
-local m = {
-    components = setmetatable({}, table_w_transform_r_cache(components))
+local m_components = setmetatable({}, table_w_transform_r_cache(components))
+
+local m = setmetatable({}, {
+    __index = function(_, k)
+        if k == "components" then
+            return m_components
+        end
+    end,
+    __newindex = function(_, k, v)
+        if k == "components" then
+            for ck, cv in pairs(v) do
+                m_components[ck] = cv
+            end
+        end
+    end,
+})
+
+defaults.components.bufname = function()
+    print("called bufname")
+    return vim.fn.bufname()
+end
+defaults.components.filename = function()
+    print("called filename")
+    return vim.fn.fnamemodify(m.components.bufname, ":t")
+end
+
+
+m.components = {
+    a = function()
+        print("called a")
+        return 5
+    end,
+    b = function()
+        print("called b")
+        return vim.fn.fnamemodify(m.components.bufname, ":h")
+    end
 }
-
-m.components.a = function()
-    print("called a")
-    return 5
-end
-
-m.components.b = function()
-    print("called b")
-    return m.components.a + 3
-end
 
 
 print(m.components.b)
 print(m.components.b)
 print(m.components.a)
+print(m.components.bufname)
+print(m.components.filename)
