29d5945c1623d160ba61fb098167464dc86ce5f8
tp86 <tom0palka+tp86@gmail.com>
2020-12-09 14:23:07 +0100

    Rewrite tests to use luaunit

--- lua/satiable2/test.lua
+++ lua/satiable2/test.lua
@@ -1,91 +1,125 @@
-vim.api.nvim_command('LuaReload satiable2')
 vim.api.nvim_command('LuaReload satiable2.dynamic_access_table')
 vim.api.nvim_command('LuaReload satiable2.utils')
 
+local lu = require'luaunit'
+
+local len = require'satiable2.utils'.len
+
 local satiable = require'satiable2'
 
---  `satiable.statusline` is a table
-assert (type(satiable.statusline) == 'table',
-        '`satiable.statusline` should be a table')
-
---  `satiable.statusline` contains existing `items` table
-assert (satiable.statusline.items and type(satiable.statusline.items) == 'table',
-        '`satiable.statusline.items` should be a table')
-
---  functions added to `items` should be accessible from functions defined later
-local items = satiable.statusline.items
-items.a = function()
-    print('called a')
-    return 'a'
+local function module_cleanup(module)
+    package.loaded[module] = nil
+    return require(module)
 end
-items.b = function()
-    print('called b')
-    return items.a() .. 'b'
+
+TestStatusline = {}
+function TestStatusline:setUp()
+    local satiable = module_cleanup('satiable2')
+    self.statusline = satiable.statusline
 end
-assert (satiable.statusline.items.b() == 'ab',
-        '`items` functions should be able to call other `items` functions defined before')
 
---  defining whole `satiable.statusline.items` table overwrites existing functions
-satiable.statusline.items = {} -- items = {} would overwrite local variable
+function TestStatusline:test_statusline_is_a_table()
+    lu.assert_is_table(self.statusline, 'statusline should be a table')
+end
 
---  but following is possible
-local statusline = satiable.statusline
-statusline.items = {}
-assert (not(satiable.statusline.items.a or satiable.statusline.items.b),
-        'defining whole `satiable.statusline.items` table should overwrite earlier functions')
+function TestStatusline:test_statusline_is_callable()
+    lu.assert_true(pcall(self.statusline), 'statusline should be callable')
+end
 
---  `items` functions can be defined together in a table
-satiable.statusline.items = {
-    c = function()
-        print('called c')
-        return 'c'
-    end,
-    d = function()
-        print('called d')
-        return items.c() .. 'd'
+TestStatuslineStructure = {}
+function TestStatuslineStructure:setUp()
+    local satiable = module_cleanup('satiable2')
+    self.satiable = satiable
+    self.statusline = satiable.statusline
+end
+
+function TestStatuslineStructure:test_statusline_contains_items_table()
+    lu.assert_eval_to_true(self.statusline.items, 'statusline should contain items')
+    lu.assert_is_table(self.statusline.items, 'statusline.items should be a table')
+end
+
+function TestStatuslineStructure:test_statusline_contains_tables_indexed_with_numbers()
+    self.statusline[1] = {}
+    lu.assert_error_msg_contains('statusline configuration must be a table', function() self.statusline[1] = 1 end,
+        'statusline should not accept values other than tables for number-indexes')
+    self.satiable.statusline = {
+        { 'a' },
+        { 'b' },
+    }
+    lu.assert_equals(len(self.statusline), 2, 'assigning multiple tables to statusline should overwrite previous configuration')
+end
+
+TestItems = {}
+function TestItems:setUp()
+    local satiable = module_cleanup('satiable2')
+    self.satiable = satiable
+    self.items = satiable.statusline.items
+end
+
+function TestItems:test_items_functions_can_call_items_defined_earlier()
+    local called = {}
+    self.items.a = function()
+        called.a = true
+        return 'a'
     end
-}
-assert (satiable.statusline.items.c() == 'c',
-        '`items` functions are able to be defined together in a table')
-
---  functions in the `items` table can still reference (via local variable with short name) to functions defined earlier in the table
-assert (satiable.statusline.items.d() == 'cd',
-        '`items` functions defined in the table should be able to reference other functions from table defined earlier')
-
---  function in the `items` table can still reference (via local variable with short name) to functions defined earlier through assignment
-items.e = function()
-    print('called e')
-    return 'e'
-end
-items.f = function()
-    print('called f')
-    return items.c() .. items.e() .. 'f'
-end
-assert (satiable.statusline.items.f() == 'cef',
-        '`items` functions defined in the table should be able to reference other function defined through assignment')
-
---  `satiable.statusline` table stores number-indexed tables (beside named tables)
-for i, t in ipairs(satiable.statusline) do
-    assert (type(t) == 'table')
+    self.items.b = function()
+        called.b = true
+        return self.items.a() .. 'b'
+    end
+    local result = self.satiable.statusline.items.b()
+    lu.assert_equals(result, 'ab', 'functions in items should refer to earlier items functions')
+    lu.assert_true(called.b, 'function in items should be called')
+    lu.assert_true(called.a, 'dependent function in items should be called')
 end
 
---  only tables can be assigned to number-indexed entries of `satiable.statusline`
-assert (pcall(function() satiable.statusline[1] = {} end),
-        'assigning to number indexes in `satiable.statusline` should accept only tables')
-assert (not pcall(function() satiable.statusline[1] = 'some value' end),
-        'assigning to number indexes in `satiable.statusline` should accept only tables')
-assert (type(satiable.statusline[1]) == 'table',
-        'accessing number index of `satiable.statusline` returns the table')
+function TestItems:test_assigning_to_items_table_overwrites_items()
+    self.items.a = function()
+        return 'a'
+    end
+    local statusline = self.satiable.statusline
+    -- cannot assign to items directly as this would overwrite local variable
+    statusline.items = {
+        b = function()
+            return 'b'
+        end
+    }
+    lu.assert_nil(self.satiable.statusline.items.a)
+    lu.assert_not_nil(self.satiable.statusline.items.b)
+end
 
---  statusline configurations are best assigned as a whole table
-satiable.statusline = {
-    { 'a' },
-    { 'b' },
-}
---  #satiable.statusline or `[i]pairs(satiable.statusline)` won't work here (requires Lua 5.2)
-assert (require'satiable2.utils'.len(satiable.statusline) == 2,
-        'assigning new table of tables to `satiable.statusline` overwrites previous assignments')
+function TestItems:test_define_multiple_functions_together()
+    local called = {}
+    local statusline = self.satiable.statusline
+    local items = self.items  -- can refer to items like this (as long as `items` is not re-assigned
+    statusline.items = {
+        a = function()
+            called.a = true
+            return 'a'
+        end,
+        b = function()
+            called.b = true
+            return items.a() .. 'b'
+        end
+    }
+    local result = self.satiable.statusline.items.b()
+    lu.assert_equals(result, 'ab', 'functions in items should refer to earlier items functions')
+    lu.assert_true(called.b, 'function in items should be called')
+    lu.assert_true(called.a, 'dependent function in items should be called')
+    items.c = function()
+        called.c = true
+        return items.a() .. 'c'
+    end
+    called = {}
+    result = self.satiable.statusline.items.c()
+    lu.assert_equals(result, 'ac', 'functions in items should refer to earlier items functions')
+    lu.assert_true(called.c, 'function in items should be called')
+    lu.assert_true(called.a, 'dependent function in items should be called')
+end
 
---  `satiable.statusline` can be called
-assert (pcall(satiable.statusline),
-        '`satiable.statusline` should be callable')
+lu.LuaUnit.run()
+
+-- cleanup after running tests
+TestStatusline = nil
+TestStatuslineStructure = nil
+TestItems = nil
+package.loaded.luaunit = nil
