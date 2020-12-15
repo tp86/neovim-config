6b98c8d0d735ee2885bbde5cceb662bb118688b7
tp86 <tom0palka+tp86@gmail.com>
2020-12-15 15:57:38 +0100

    Statusline basic configuration

--- colors/my_colors.vim
+++ colors/my_colors.vim
@@ -1,14 +1,38 @@
 runtime colors/morning.vim
 
-let &fillchars = "vert: "
-highlight! link VertSplit StatusLineNC
-highlight! link Folded FoldColumn
 function! s:get_attr(group, attr)
   return synIDattr(synIDtrans(highlightID(a:group)), a:attr, "gui")
 endfunction
-function! s:dim(color, by)
+function! s:darken(color, by)
   return "#"..printf("%06x", (nvim_get_color_by_name(a:color) - nvim_get_color_by_name(a:by)))
 endfunction
-execute "highlight ".."SignColumn".." guibg".."="..s:dim(s:get_attr("Normal", "bg"), "#080808")
+function! s:lighten(color, by)
+  return "#"..printf("%06x", (nvim_get_color_by_name(a:color) + nvim_get_color_by_name(a:by)))
+endfunction
+
+let &fillchars = "vert: "
+highlight! link VertSplit StatusLineNC
+highlight! link Folded FoldColumn
+execute "highlight ".."SignColumn".." guibg".."="..s:darken(s:get_attr("Normal", "bg"), "#080808")
+
+" statusline colors
+"let s:stl_fg = synIDattr(synIDtrans(highlightID("StatusLine")), "fg", "gui")
+let s:stl_bg = s:lighten(s:get_attr("Normal", "bg"), "#101010")
+let s:stl_nc_bg = s:lighten("Black", "#606060")
+execute "highlight! ".."StatusLine".." gui=NONE guifg=Black guibg="..s:stl_bg
+execute "highlight! ".."StatusLineNC".." gui=NONE guifg=LightGray guibg="..s:stl_nc_bg
+
+let s:stl_cwd_fg = "#399cbd"
+execute "highlight! ".."StlCwd".." guifg="..s:stl_cwd_fg.." gui=bold guibg="..s:stl_bg
+execute "highlight! ".."StlNCCwd".." guifg="..s:stl_cwd_fg.." gui=bold guibg="..s:stl_nc_bg
+
+let s:stl_fname_mod_fg = "#ff2727"
+let s:stl_fname_ro_fg = "#00c400"
+execute "highlight! ".."StlFname".." gui=bold guifg=".."Black".." guibg="..s:stl_bg
+execute "highlight! ".."StlNCFname".." gui=bold guifg=".."LightGray".." guibg="..s:stl_nc_bg
+execute "highlight! ".."StlFnameMod".." gui=bold guifg="..s:stl_fname_mod_fg.." guibg="..s:stl_bg
+execute "highlight! ".."StlNCFnameMod".." gui=bold guifg="..s:stl_fname_mod_fg.." guibg="..s:stl_nc_bg
+execute "highlight! ".."StlFnameRo".." gui=bold guifg="..s:stl_fname_ro_fg.." guibg="..s:stl_bg
+execute "highlight! ".."StlNCFnameRo".." gui=bold guifg="..s:stl_fname_ro_fg.." guibg="..s:stl_nc_bg
 
 let g:colors_name = "my_colors"
--- lua/stl_tbl.lua
+++ lua/stl_tbl.lua
@@ -1,97 +1,189 @@
 local m = {}
-m.items = {
-    filename = function()
-        return vim.fn.fnamemodify(m.items.bufname(), ':t')
-    end,
-    bufname = function()
-        return vim.fn.bufname()
-    end,
-}
-local conditional_item = {
-    when = function()
-        return vim.fn.fnamemodify(m.items.bufname(), ':h') == 'lua'
-    end,
-    m.items.filename
-}
-local function convert_conditional_item(item, f)
-    if item.when then
-        return function()
-            if item.when() then
-                return item[1]()
-            end
-            return ''
+local function make_hl_func(hl_def, f)
+    return function()
+        if hl_def.when() then
+            return f()
         end
+        return ''
     end
-    return f
 end
-local highlighted_item = {
-    highlight = {
-        {
-            when = function()
-                return vim.api.nvim_buf_get_option(0, 'modified')
-            end,
-            'WarningMsg'
-        },
-        {
-            when = function()
-                return not vim.api.nvim_buf_get_option(0, 'modified')
-            end,
-            'String'
-        },
-    },
-    m.items.filename
-}
-local function convert_highlighted_item(item, f)
-    if item.highlight then
-        local highlights = {}
-        for i, hl_tbl in ipairs(item.highlight) do
-            table.insert(highlights, '%#'..hl_tbl[1]..'#')
-            local hl_func = function()
-                if hl_tbl.when() then -- TODO handle unconditional highlight
+local function add_hl_item(hl_tbl, hl_group)
+    table.insert(hl_tbl, '%#'..hl_group..'#')
+end
+local function add_hl_func(hl_tbl, f_name)
+    table.insert(hl_tbl, [[%{luaeval("require'stl_tbl'.items.]]..f_name..[[()")}]])
+end
+local converters = {
+    function(item, f) -- conditional
+        if item.when then
+            return function()
+                if item.when() then
                     return f()
                 end
                 return ''
             end
-            m.items['hl'..i] = hl_func
-            table.insert(highlights, 'luaeval require...items.hl'..i)
         end
-        return table.concat(highlights, '')
-    end
-    return f()
-end
-m.ci = convert_conditional_item(conditional_item, conditional_item[1])
-m.hi = convert_highlighted_item(highlighted_item, highlighted_item[1])
-local function converter(item)
-    local func = item[1]
-    local converters = {
-        convert_conditional_item,
-        convert_highlighted_item,
-    }
-    local value = func
-    for _, converter in ipairs(converters) do
-        value = converter(item, value)
-    end
-    return value
-end
-local cond_hl_item = {
-    when = function()
-        return vim.fn.fnamemodify(m.items.bufname(), ':h') == 'lua'
+        return f
     end,
-    highlight = {
-        {
-            when = function()
-                return vim.api.nvim_buf_get_option(0, 'modified')
-            end,
-            'WarningMsg'
-        },
-        {
-            when = function()
-                return not vim.api.nvim_buf_get_option(0, 'modified')
-            end,
-            'String'
-        },
-    },
-    m.items.filename
+    function(item, f, item_index) -- highlights
+        if item.highlight then
+            local highlights = {}
+            if type(item.highlight) == 'table' then
+                for i, hl_def in ipairs(item.highlight) do
+                    add_hl_item(highlights, hl_def[1])
+                    local func_name = '_'..item_index..'_'..i
+                    m.items[func_name] = make_hl_func(hl_def, f)
+                    add_hl_func(highlights, func_name)
+                end
+            elseif type(item.highlight) == 'string' then
+                add_hl_item(highlights, item.highlight)
+                local func_name = '_'..item_index..'_' .. 0
+                m.items[func_name] = function() return f() end
+                add_hl_func(highlights, func_name)
+            end
+            table.insert(highlights, '%*')
+            return table.concat(highlights, '')
+        end
+        return f()
+    end,
+    -- TODO add format
 }
-m.c = converter(cond_hl_item)
+local function lookup_func_name(tbl, func)
+    for name, value in pairs(tbl) do
+        if value == func then
+            return name
+        end
+    end
+end
+local item_type_dispatcher = {
+    ['table'] = function(item, item_index)
+        local group_func = function()
+            local group = {}
+            for _, item in ipairs(item) do
+                if type(item) == 'string' then
+                    table.insert(group, item)
+                elseif type(item) == 'function' then
+                    table.insert(group, item())
+                end
+            end
+            return table.concat(group, '')
+        end
+        local value = group_func
+        for _, converter in ipairs(converters) do
+            value = converter(item, value, item_index)
+        end
+        return value
+    end,
+    ['string'] = function(item)
+        return item
+    end,
+    ['function'] = function(item)
+        local func_name = lookup_func_name(m.items, item)
+        return [[%{luaeval("require'stl_tbl'.items.]]..func_name..[[()")}]]
+    end
+}
+local function converter(item, item_index)
+    return item_type_dispatcher[type(item)](item, item_index)
+end
+
+local conditions = {}
+conditions.active = function()
+    return vim.api.nvim_get_var('actual_curwin') == string.format('%d', vim.fn.win_getid())
+end
+conditions.inactive = function()
+    return not conditions.active()
+end
+conditions.mod = function()
+    return vim.api.nvim_buf_get_option(0, 'modified')
+end
+conditions.nomod = function()
+    return not conditions.mod()
+end
+conditions.ro = function()
+    return vim.api.nvim_buf_get_option(0, 'readonly')
+end
+conditions.noro = function()
+    return not vim.api.nvim_buf_get_option(0, 'readonly')
+end
+conditions.mod_active = function()
+    return conditions.active() and conditions.mod()
+end
+conditions.mod_inactive = function()
+    return conditions.inactive() and conditions.mod()
+end
+conditions.ro_active = function()
+    return conditions.active() and conditions.nomod() and conditions.ro()
+end
+
+conditions.ro_inactive = function()
+    return conditions.inactive() and conditions.nomod() and conditions.ro()
+end
+conditions.fname_active = function()
+    return conditions.active() and conditions.nomod() and conditions.noro()
+end
+conditions.fname_inactive = function()
+    return conditions.inactive() and conditions.nomod() and conditions.noro()
+end
+m.items = {
+    bufname = function()
+        return vim.fn.bufname()
+    end,
+    bufname_full = function()
+        return vim.fn.fnamemodify(m.items.bufname(), ':p')
+    end,
+    filename = function()
+        return vim.fn.fnamemodify(m.items.bufname(), ':t')
+    end,
+    cwd = function()
+        return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+    end,
+    cwd_shortened = function()
+        return vim.fn.pathshorten(m.items.cwd())
+    end,
+    file_path_relative = function()
+        local full_cwd = vim.fn.escape(vim.fn.fnamemodify(m.items.cwd(), ':p'), [[\%]])
+        local relative_path = vim.fn.matchstr(m.items.bufname_full(), [[\v]]..full_cwd..[[\zs.*$]])
+        if #relative_path == 0 then
+            relative_path = m.items.bufname_full()
+        end
+        local relative_dir = vim.fn.fnamemodify(relative_path, ':h')
+        if relative_dir == '.' then
+            return m.items.filename()
+        else
+            return vim.fn.expand(vim.fn.pathshorten(relative_dir)..'/'..m.items.filename())
+        end
+    end
+}
+local statusline = {
+    {
+        when = conditions.active,
+        highlight = 'StlCwd',
+        m.items.cwd_shortened,
+        ': ',
+    },
+    {
+        highlight = {
+            { when = conditions.mod_active, 'StlFnameMod' },
+            { when = conditions.mod_inactive, 'StlNCFnameMod' },
+            { when = conditions.ro_active, 'StlFnameRo' },
+            { when = conditions.ro_inactive, 'StlNCFnameRo' },
+            { when = conditions.fname_active, 'StlFname' },
+            { when = conditions.fname_inactive, 'StlNCFname' },
+
+        },
+        m.items.file_path_relative
+    },
+    '%=',
+    '%l(%L):%-3c',
+}
+
+m.statusline = setmetatable(statusline, {
+    __call = function(tbl)
+        local items = {}
+        for i, item in ipairs(tbl) do
+            table.insert(items, converter(item, i))
+        end
+        return table.concat(items, '')
+    end
+})
 return m
