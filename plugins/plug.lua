9d37cc59a4ea914759588999d979e52fc134f678
tp86 <tom0palka+tp86@gmail.com>
2023-10-11 16:20:59 +0200

    Working version of automatic plugin system with plug

--- plugins/plug.lua
+++ plugins/plug.lua
@@ -1,35 +1,5 @@
 -- inspired by https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
 
-do -- bootstrap
-  local plug_file = vim.fn.stdpath("config") .. "/autoload/plug.vim"
-  local function sync()
-    vim.call("plug#begin") -- force sourcing of autoload
-    vim.cmd [[ PlugInstall --sync ]]
-    require("plugins.plug", true)
-  end
-  if not vim.loop.fs_stat(plug_file) then
-    vim.loop.spawn("curl", {
-      args = { "--create-dirs", "-fLo", plug_file, "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"},
-    },
-    function(code, signal)
-      if code == 0 then
-        if vim.v.vim_did_enter == 0 then -- still during startup
-          vim.defer_fn(function()
-            vim.api.nvim_create_autocmd("VimEnter", {
-              callback = sync,
-            })
-          end, 0)
-        else
-          vim.defer_fn(function()
-            sync()
-          end, 0)
-        end
-      end
-    end)
-    return
-  end
-end
-
 local translate_key = setmetatable({
   build = "do",
 }, {
@@ -66,7 +36,8 @@ local function setup_plugins(plugins)
     local repo = plugin[1]
     local opts = get_options(plugin)
     plug(repo, opts)
-    if type(plugin.config) == "function" then
+    local config = plugin.config
+    if type(config) == "function" then
       local name = plugin.as or get_name(plugin)
       if is_lazy(plugin) then
         vim.api.nvim_create_autocmd("User", {
@@ -80,9 +51,71 @@ local function setup_plugins(plugins)
     end
   end
   vim.fn["plug#end"]()
-  for _, config_fn in ipairs(start_configs) do
+  return start_configs
+end
+
+local function configure_plugins(configs)
+  for _, config_fn in ipairs(configs) do
     config_fn()
   end
 end
 
-setup_plugins{}
+local function setup()
+  local plugins = {
+    { "EdenEast/nightfox.nvim",
+      config = function()
+        require("nightfox").setup {
+          options = {
+            styles = {
+              comments = "",
+            },
+          },
+        }
+        vim.cmd("colorscheme duskfox")
+      end,
+    },
+  }
+  local configs = setup_plugins(plugins)
+  local missing = false
+  for _, plugin in pairs(vim.g.plugs) do
+    if vim.fn.isdirectory(plugin.dir) == 0 then
+      missing = true
+      break
+    end
+  end
+  return configs, missing
+end
+
+-- bootstrap
+local plug_file = vim.fn.stdpath("config") .. "/autoload/plug.vim"
+
+local function sync()
+  local configs, changed = setup()
+  if changed then
+    vim.cmd("PlugInstall --sync")
+  end
+  configure_plugins(configs)
+end
+
+if vim.loop.fs_stat(plug_file) then
+  sync()
+else
+  vim.loop.spawn("curl", {
+    args = { "--create-dirs", "-fLo", plug_file, "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"},
+  },
+  function(code, signal)
+    if code == 0 then
+      if vim.v.vim_did_enter == 0 then -- still during startup
+        vim.defer_fn(function()
+          vim.api.nvim_create_autocmd("VimEnter", {
+            callback = sync,
+          })
+        end, 0)
+      else
+        vim.defer_fn(function()
+          sync()
+        end, 0)
+      end
+    end
+  end)
+end
