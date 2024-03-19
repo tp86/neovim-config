21343b838c3db527b8cb008638fa362dfab9c4d7
tp86 <tom0palka+tp86@gmail.com>
2024-03-19 13:31:25 +0100

    Improve keybindings

--- lua/plugins/nvim-extensions.lua
+++ lua/plugins/nvim-extensions.lua
@@ -170,10 +170,10 @@ local plugins = {
       vim.fn.sign_define("DapStopped", { text = '▶', texthl = '', linehl = '', numhl = '' })
       local nmap = require("common").map.n
       local dap = require("dap")
-      nmap("<c-7>", dap.continue)
-      nmap("<c-8>", dap.step_over)
-      nmap("<c-9>", dap.step_into)
-      nmap("<c-0>", dap.step_out)
+      nmap("<a-7>", dap.continue)
+      nmap("<a-8>", dap.step_over)
+      nmap("<a-9>", dap.step_into)
+      nmap("<a-0>", dap.step_out)
       nmap("<localleader>b", dap.toggle_breakpoint)
     end,
   },
