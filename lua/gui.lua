e72db07c6f0cc721e0df6fc48a1896cfa7fce996
tp86 <tom0palka+tp86@gmail.com>
2024-03-23 13:50:16 +0100

    Set guifont from variable to make it modifiable

--- lua/gui.lua
+++ lua/gui.lua
@@ -8,8 +8,10 @@ local group = vim.api.nvim_create_augroup("GuiConfig", { clear = true })
 vim.api.nvim_create_autocmd("UIEnter", {
   group = group,
   callback = function()
-    execute_if_command_exists("GuiFont! Hack:h14")
+    execute_if_command_exists("GuiFont! " .. vim.g.guifont)
     vim.opt.guicursor = [[n-v-c-sm:block-Cursor,i-ci-ve:ver25-blinkwait200-blinkon500-blinkoff500,r-cr-o:hor20]]
     vim.opt.mouse = "a"
   end,
 })
+
+vim.g.guifont = "Hack:h14"
