7dd4f7956ae282dcc1086d27e311d8a263469ba4
tp86 <tom0palka+tp86@gmail.com>
2024-03-28 09:48:42 +0100

    Set GUI window title to global working directory

--- lua/gui.lua
+++ lua/gui.lua
@@ -4,14 +4,27 @@ local function execute_if_command_exists(command)
   end
 end
 
-local group = vim.api.nvim_create_augroup("GuiConfig", { clear = true })
-vim.api.nvim_create_autocmd("UIEnter", {
-  group = group,
-  callback = function()
-    execute_if_command_exists("GuiFont! " .. vim.g.guifont)
-    vim.opt.guicursor = [[n-v-c-sm:block-Cursor,i-ci-ve:ver25-blinkwait200-blinkon500-blinkoff500,r-cr-o:hor20]]
-    vim.opt.mouse = "a"
-  end,
+local register_autocmds_group = require("common").register_autocmds_group
+register_autocmds_group("GuiConfig", {
+  {
+    "UIEnter",
+    callback = function()
+      if vim.g.GuiLoaded then
+        execute_if_command_exists("GuiFont! " .. vim.g.guifont)
+        vim.opt.guicursor = [[n-v-c-sm:block-Cursor,i-ci-ve:ver25-blinkwait200-blinkon500-blinkoff500,r-cr-o:hor20]]
+        vim.opt.mouse = "a"
+      end
+    end,
+  },
+  {
+    "DirChanged",
+    pattern = "global",
+    callback = function(ev)
+      vim.opt.titlestring = ev.file
+    end,
+  }
 })
 
 vim.g.guifont = "Hack:h14"
+vim.opt.title = true
+vim.opt.titlestring = vim.fn.getenv("PWD")
